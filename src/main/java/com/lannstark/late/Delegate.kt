93c9868b6fdc929b28ce8e7cc8c1d862e12aaabe
MYUNGHOON JU <myunghoonju@gmail.com>
2024-03-23 21:27:32 +0900

    add delegate funcs

--- src/main/java/com/lannstark/late/Delegate.kt
+++ src/main/java/com/lannstark/late/Delegate.kt
@@ -1,8 +1,19 @@
 package com.lannstark.late
 
+import kotlin.properties.Delegates
+
 fun main() {
-    val person = Person()
-    person.confirmKim
+    val person5 = Person5()
+    val person6 = Person6()
+
+    person5.age = 30
+    println(person5.age)
+    person5.age = 0
+    println(person5.age)
+
+    person6.num = 10
+    println(person6.num)
+    println(person6.age)
 }
 
 class Person {
@@ -38,3 +49,23 @@ class PersonThree {
         "Kim"
     }
 }
+
+class Person4 {
+    var age: Int by Delegates.observable(20) { _, oldValue, newValue ->
+        if (oldValue != newValue) {
+            println("before ${oldValue} after : ${newValue}")
+        }
+    }
+}
+
+class Person5 {
+    var age: Int by Delegates.vetoable(20) { _, _, newValue -> newValue >= 1}
+}
+
+class Person6 {
+    @Deprecated("use age", ReplaceWith("age"))
+    var num: Int = 0
+
+    var age: Int by this::num
+}
+
